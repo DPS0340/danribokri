bbd86c20b9b27851c06c21d5059ce089e95eb4ba
DPS0340 <jh001007@gmail.com>
2019-06-23 21:57:26 +0900

    fixed

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function run() {
-    let money = +document.getElementById('original').value;
-    let year = +document.getElementById('year').value;
-    let ratio = +document.getElementById('ratio').value;
-    let result = document.getElementById("result");
-    result.innerText = String(money*((1+(ratio/100))**year));
-}
-function run2() {
-    let money = +document.getElementById('original').value;
-    let year = +document.getElementById('year').value;
-    let ratio = +document.getElementById('ratio').value;
-    let result = document.getElementById("result");
-    result.innerText = String(money+money*(ratio/100)*year);
-}
+$(function() {
+    function danri() {
+        let money = +$("#original").val();
+        let year = +$("#year").val();
+        let ratio = +$("#ratio").val();
+        $("#result").text(String(money + money * (ratio / 100) * year));
+    }
+    function bokri() {
+        let money = +$("#original").val();
+        let year = +$("#year").val();
+        let ratio = +$("#ratio").val();
+        $("#result").text(String(money*((1+(ratio/100))**year)));
+    }
+    $("#submit").click(danri);
+    $("#submit2").click(bokri);
+});

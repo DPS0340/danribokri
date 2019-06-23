d8e41ec3f40ee3ec3519a0671212bf1c4496d7d8
DPS0340 <jh001007@gmail.com>
2019-06-24 06:44:22 +0900

    hot fixed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function() {
         let money = +$("#original").val();
         let year = +$("#year").val();
         let ratio = +$("#ratio").val();
-        $("#result").text(String(money + money * (ratio / 100) * year).toFixed(0));
+        $("#result").text(String((money + money * (ratio / 100) * year).toFixed(0)));
     }
     function bokri() {
         let money = +$("#original").val();

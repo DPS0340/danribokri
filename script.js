14633d24c3f3031738e6f7f0aba5348e5f26df04
DPS0340 <jh001007@gmail.com>
2019-06-23 18:27:15 +0900

    단리계산 추가

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function run() {
     let result = document.getElementById("result");
     result.innerText = String(money*((1+(ratio/100))**year));
 }
+function run2() {
+    let money = +document.getElementById('original').value;
+    let year = +document.getElementById('year').value;
+    let ratio = +document.getElementById('ratio').value;
+    let result = document.getElementById("result");
+    result.innerText = String(money+money*(ratio/100)*year);
+}

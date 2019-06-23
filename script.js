90c831ae3dba3e69b92158cd52905c91cef2dfa7
DPS0340 <jh001007@gmail.com>
2019-06-23 18:22:52 +0900

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+function run() {
+    let money = +document.getElementById('original').value;
+    let year = +document.getElementById('year').value;
+    let ratio = +document.getElementById('ratio').value;
+    let result = document.getElementById("result");
+    result.innerText = String(money*((1+(ratio/100))**year));
+}

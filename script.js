56690dd02cb1395c0b2ee4891060b9abb418cc22
DPS0340 <jh001007@gmail.com>
2019-06-24 06:41:44 +0900

    fixed point -> zero

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ $(function() {
         let money = +$("#original").val();
         let year = +$("#year").val();
         let ratio = +$("#ratio").val();
-        $("#result").text(String(money + money * (ratio / 100) * year));
+        $("#result").text(String(money + money * (ratio / 100) * year).toFixed(0));
     }
     function bokri() {
         let money = +$("#original").val();
         let year = +$("#year").val();
         let ratio = +$("#ratio").val();
-        $("#result").text(String(money*((1+(ratio/100))**year)));
+        $("#result").text(String((money*((1+(ratio/100))**year)).toFixed(0)));
     }
     $("#submit").click(danri);
     $("#submit2").click(bokri);
